20a823672c50aeb5f88bc2a9af71b9bc42da99f4
Kevin Sekin <samp-sit@web.de>
2018-07-13 06:08:08 +0200

    Implemented wrapper to the functionality that arangoDB provides for testing

--- src/revisions.js
+++ src/revisions.js
@@ -6,3 +6,133 @@
  * ===========================
  */
 
+const db = require('@arangodb').db;
+
+module.exports = revision => {
+	return {
+		revisionCollectionName: (name) => {
+			return module.context.collectionName(name);
+		},
+		revisionCollectionHistoryName: (name) => {
+			return module.context.collectionName(name + '__internal__history');
+		},
+		revisionCollectionEdgeName: (name) => {
+			return module.context.collectionName(name + '__internal__history__edges');
+		},
+		create: (name) => {
+			const collectionName = this.revisionCollectionName(name);
+			const collectionHistoryName = this.revisionCollectionHistoryName(name);
+			const collectionEdgeName = this.revisionCollectionEdgeName(name);
+			
+			if (db._collection(collectionName) ||
+				db._collection(collectionHistoryName) ||
+				db._collection(collectionEdgeName)) {
+				throw new Error('The revision already exists');
+			} else {
+				db._createDocumentCollection(collectionHistoryName);
+				db._createEdgeCollection(collectionEdgeName);
+				return db._createDocumentCollection(collectionName);
+			}
+		},
+		revisionCollection: (name) => {
+			let collection = db.collection(this.revisionCollectionName(name));
+			let revisionCollection = {
+				all: () => {
+					return this.collection.all();
+				},
+				byExample: (example) => {
+					return this.collection.byExample(example);
+				},
+				firstExample: (example) => {
+					return this.collection.firstExample(example);
+				},
+				range: (attribute, left, right) => {
+					return this.collection.range(attribute, left, right);
+				},
+				closedRange: (attribute, left, right) => {
+					return this.collection.closedRange(attribute, left, right);
+				},
+				any: () => {
+					return this.collection.any();
+				},
+				count: () => {
+					return this.collection.count();
+				},
+				toArray: () => {
+					return this.collection.toArray();
+				},
+				document: (handle) => {
+					return this.collection.document(handle);
+				},
+				documents: (handles) => {
+					return this.collection.documents(handles);
+				},
+				type: () => {
+					return this.collection.type();
+				},
+				iterate: (iterator, options) => {
+					return this.collection.iterate(iterator, options);
+				},
+				exists: (handle) => {
+					return this.collection.exists(handle);
+				},
+				insert: (object, options = {}) => {
+				
+				},
+				replace: (handle, object, options = {}) => {
+				
+				},
+				update: (handle, object, options = {}) => {
+				
+				},
+				remove: (handle, options) => {
+				
+				},
+				removeByKeys: (handles) => {
+				
+				},
+				removeByExample: (example) => {
+				
+				},
+				replaceByExample: (example, object) => {
+				
+				},
+				updateByExample: (example, object) => {
+				
+				},
+				revisions: (handle) => {
+				
+				},
+				previous: (handle, revision) => {
+				
+				},
+				next: (handle, revision) => {
+				
+				},
+				revisionByDate: (dateOfInterest) => {
+				
+				},
+				revisionByDateRange: (dateRangeMin, dateRangeMax) => {
+				
+				}
+			};
+			return Object.assign(revisionCollection, {collection: collection});
+		},
+		revisionEdgeCollection: (name) => {
+			let edgeCollection = db.collection(this.revisionCollectionName(name));
+			let edgeRevisionCollection = {
+				edges: (handle) => {
+					return this.collection.edges(handle);
+				},
+				inEdges: (handle) => {
+					return this.collection.inEdges(handle);
+				},
+				outEdges: (handle) => {
+					return this.collection.outEdges(handle);
+				}
+			};
+			// Slight hack by replacing collection with the edge collection I can still use the document collection functions...
+			return Object.assign(this.revisionCollection(name), {edgeRevisionCollection, collection: edgeCollection});
+		}
+	}
+}
