7ac5e892667a60c8111234f8bbe02f064c973911
KevinExtremo <samp-sit@web.de>
2018-10-13 12:16:18 +0200

    Added a migration script that will migrate the older version of the timetravel framework to the new one since I introduced the breaking change with the proxies

--- src/timetravelinfo.js
+++ src/timetravelinfo.js
@@ -6,7 +6,7 @@
  * ===========================
  */
 
-const Version = "v1.0.0";
+const Version = "v1.1.0";
 const SettingsCollection = "timetravel_settings__internal";
 const MaxTime = 8640000000000000;
 
